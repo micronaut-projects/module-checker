ed0ae3dbddc165b8386a26904e09ac68deab9670
Tim Yates <tim.yates@gmail.com>
2022-12-13 12:02:34 +0000

    Skip in-progress actions

--- src/main/kotlin/module/checker/GithubActions.kt
+++ src/main/kotlin/module/checker/GithubActions.kt
@@ -4,11 +4,11 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import io.micronaut.serde.annotation.Serdeable
 
 @Serdeable
-data class GithubAction(val name: String, val conclusion: String)
+data class GithubAction(val name: String, val conclusion: String?)
 
 @Serdeable
 data class GithubActions(
     @field:JsonProperty("workflow_runs") val runs: List<GithubAction>
 ) {
-    fun latestJavaCi() = runs.firstOrNull { it.name == "Java CI" }?.conclusion ?: "unknown"
+    fun latestJavaCi() = runs.firstOrNull { it.name == "Java CI" && it.conclusion != null }?.conclusion ?: "unknown"
 }
